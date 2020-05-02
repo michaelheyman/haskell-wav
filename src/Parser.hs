c568510645d96bd89bad50e930513ba343dcaed9
Michael Heyman <michaelheyman@users.noreply.github.com>
2020-05-02 16:50:18 -0700

    Parse riff chunk

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parser where
 
-import           Data.Attoparsec.ByteString (Parser)
-import           Prelude                    hiding (take)
+import           Data.Attoparsec.Binary     (anyWord32le)
+import           Data.Attoparsec.ByteString (Parser, string)
+import           Data.ByteString            (ByteString)
+import           Data.Word
 
 import           Types
 
+parseChunkID :: Parser ByteString
+parseChunkID = string "RIFF"
+
+parseChunkSize :: Parser Word32
+parseChunkSize = anyWord32le
+
 riffParser :: Parser Riff
-riffParser = undefined
+riffParser = do
+    chunkID <- string "RIFF"
+    chunkSize <- anyWord32le
+    chunkFormat <- string "WAVE"
+    return $ Riff chunkID chunkSize chunkFormat
 
 formatParser :: Parser Format
 formatParser = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.ByteString       (ByteString)
+import           Parser
+import           Test.Hspec
+import           Test.Hspec.Attoparsec
+
+specChunkId :: Spec
+specChunkId =
+    describe "parseChunkID" $
+      it "should parse the bytestring riff" $
+        parseChunkID `shouldSucceedOn` ("RIFF" :: ByteString)
+
+specChunkSize :: Spec
+specChunkSize =
+    describe "parseChunkSize" $ do
+        it "should parse the 32-bit word '0000'" $
+            parseChunkSize `shouldSucceedOn` ("0000" :: ByteString)
+        it "should parse the 32-bit word '1000'" $
+            parseChunkSize `shouldSucceedOn` ("1000" :: ByteString)
+        it "should parse the 32-bit word '5555'" $
+            parseChunkSize `shouldSucceedOn` ("5555" :: ByteString)
+        it "should parse the 32-bit word '9999'" $
+            parseChunkSize `shouldSucceedOn` ("9999" :: ByteString)
+
+specRiff :: Spec
+specRiff =
+    describe "riffParser" $ do
+        it "should parse a valid riff chunk" $
+            riffParser `shouldSucceedOn` ("RIFF1000WAVE" :: ByteString)
+        it "should leave nothing unconsumed with a valid riff chunk" $
+            ("RIFF1000WAVE" :: ByteString) ~?> riffParser `leavesUnconsumed` ""
+        it "should leave extra characters unconsumed with a valid riff chunk with extra characters" $
+            ("RIFF1000WAVES" :: ByteString) ~?> riffParser `leavesUnconsumed` "S"
+        it "should fail when all sub-chunks are missing" $
+            riffParser `shouldFailOn` ("" :: ByteString)
+        it "should fail when the chunkID does not equal 'RIFF'" $
+            riffParser `shouldFailOn` ("FFIR" :: ByteString)
+        it "should fail when the size and format chunks are missing" $
+            riffParser `shouldFailOn` ("RIFF" :: ByteString)
+        it "should fail when the size and format chunks are missing" $
+            riffParser `shouldFailOn` ("RIFF1000" :: ByteString)
+        it "should fail when the chunkFormat does not equal 'WAVE'" $
+            riffParser `shouldFailOn` ("RIFF1000OGG" :: ByteString)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    hspec specChunkId
+    hspec specChunkSize
+    hspec specRiff
